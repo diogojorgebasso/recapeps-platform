c7025d0d503abd4820fdebe7150eb4db737c8a63
Diogo Jorge Basso <65865529+diogojorgebasso@users.noreply.github.com>
2024-11-15 11:54:51 +0100

    fix: Error in the React Router Dom component. "You can use the v7_relativeSplatPath future flag to opt-in early." For making our code future-proof, those flags were inserted in BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import { Routes } from "./Routes";
 
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
+import { CookieProvider } from "./context/CookieContext";
 
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes />
+      <BrowserRouter future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}>
+        <CookieProvider>
+          <Routes />
+        </CookieProvider>
       </BrowserRouter>
     </AuthProvider>
   );
