a2a63182d52466da2fa86c10fac0b54cb22c1679
Diogo Jorge Basso <65865529+diogojorgebasso@users.noreply.github.com>
2024-11-29 11:25:50 +0100

    removed services folder for now

--- src/services/Quizz/MockDataSource.ts
+++
@@ -1,7 +0,0 @@
-import quizResultsMock from "./quizResults.json";
-
-export async function getQuizzesFromMock() {
-  // Simula um atraso na resposta para se parecer mais com uma chamada de rede
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  return quizResultsMock;
-}
