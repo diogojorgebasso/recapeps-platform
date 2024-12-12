21fb22ec7c83c0e5d7c595c6eeb6c991d6612bd3
Diogo Jorge Basso <65865529+diogojorgebasso@users.noreply.github.com>
2024-12-12 11:10:19 +0100

    feat : add pk_test

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,7 +2,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./CheckoutForm";
 
-const stripePromise = loadStripe("pk_test_YOUR_PUBLIC_KEY"); // Chave pública Stripe
+const stripePromise = loadStripe("pk_test_51IOvMXBiilgt3voLm7kvY2GAgsyYVssTyO59sttI2KqgGLTrgPMA5WHox80IXFS7YCuQuo40nXj0qX9JKZiTRocQ00l1cSSkFU");
 
 export default function Checkout() {
     return (
