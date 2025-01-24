4d69b2df37b5690acf50ef791f78841592160619
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-01-24 09:22:31 +0100

    fix: update logo dimensions in Home component for better visibility

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     return (
         <Box>
             <VStack alignItems="center" py="8" bg={bg} width="100%" textAlign="center">
-                <Image src="/logo.svg" height="0.5" width="0.5" alt="Recapeps Logo" boxSize="150px" />
+                <Image height="220px" width="220px" alt="Recapeps Logo" src="/logo.svg" />
                 <Heading as="h1" size="5xl" letterSpacing="tight">
                     <Highlight query="RECAP'EPS" styles={{ color: "orange.600" }}>
                         Bienvenue sur RECAP'EPS
