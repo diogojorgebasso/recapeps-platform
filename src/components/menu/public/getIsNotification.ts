f6ff70c9d975d5bbbfbd6429c774f12aac07198f
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-05-14 00:23:23 +0200

    WIP : removed temp menu

--- src/components/menu/public/getIsNotification.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/firebase/clientApp"
-import { collection, where, query, limit, getDocs } from "firebase/firestore"
-
-export async function getIsNotification(uid: string) {
-    const docRef = collection(db, "users", uid, "notifications")
-    const q = query(docRef, where("isRead", "==", false), limit(1))
-    const snapshot = await getDocs(q)
-    return snapshot.docs.length > 0
-}
